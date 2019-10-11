a24498510d1e76a790ee4d88b3cef77416859fc7
CahyaProject <56394451+CahyaProject@users.noreply.github.com>
2019-10-11 21:01:34 +0700

    Add files via upload

---
+++ HW.sh
@@ -0,0 +1,24 @@
+#usr/bin/bash
+clear
+#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+sh install.sh
+figlet -f slant "Bayar Goblog :v" | lolcat
+echo $i"Maaf Gan Tools Ini Bayar :v"
+sleep 2
+echo $ku"Ginjal Lu Kan Ada Jadi Jual Aja xD"
+sleep 2
+echo $bi"Tenang Gan Harganya 200K :)"
+echo $cy"Bayar Kentod Jgn Cuma Nanya Doang :v"
+echo $i"Membuka Whatsapp Admin..."
+sleep 5
+xdg-open http://wa.me/=62895389743582
+done
+
